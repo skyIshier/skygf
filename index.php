2e6c49da9af316b42edbd0534ed153b374a0242c
sky十二 <2995229674@qq.com>
2026-05-18 17:43:12 +0800

    Update and rename index.php to index.html

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$request = $_SERVER['REQUEST_URI'];
-if ($request === '/') {
-    header('Location: https://skygf.infinityfree.me/');
-} else {
-    header('Location: https://skygf.infinityfree.me' . $request);
-}
-exit;
-?>
